324aa7b935b4f53faa227d96247a96d0f9343eaf
Heemo Yang <heemo521@gmail.com>
2022-03-15 23:02:17 -0700

    Commit before push

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-//NEED TO RESET INPUT RADIO BUTTON TO NO
-
 // BOOK ARRAY
 let library = [];
 
@@ -73,7 +71,7 @@ consectetur delenitid`
 // SELECTORS
 const formModal = document.querySelector('#form');
 const overlay = document.querySelector('#overlay');
-const btnsOpenForm = document.querySelectorAll('.btn-add');
+const btnOpenForm = document.querySelector('.btn-add');
 const btnsCloseForm = [document.querySelector('.btn-close'), document.querySelector('.btn-exit'), overlay];
 const btnDelete = document.querySelector('.btn-delete');
 const booksCtn = document.querySelector('.books-container');
@@ -96,7 +94,7 @@ const displayBook = function (books) {
         const bookHtml = `
                 <div class="book" data-id="${id}">
                 <div class="book-top">
-                    <h3 class="title"><a href="#" class="btn btn-edit btn-add">${title}</a></h3>
+                    <h3 class="title"><a href="#" class="btn btn-edit">${title}</a></h3>
                 </div>
 
                 <div class="book-info">
@@ -147,6 +145,15 @@ const openForm = function () {
     overlay.classList.remove('hidden');
 };
 
+//FUNCTION: REOPEN FORM
+// BOOK-TITLE-BTN TO OPEN UP THE FORM AGAIN TO BE ABLE TO EDIT THE CARD
+// FORM SHOULD ALREADY BE FILLED OUT WITH SAVED DATA
+// const reOpenForm = function (event) {
+//     event.target
+//     setFormValues(book);
+//     openForm();
+// };
+
 //FUNCTION: CLOSE THE FORM
 const closeForm = function () {
     formModal.classList.add('hidden');
@@ -159,8 +166,6 @@ const closeForm2 = function (event) {
         closeForm();
     }
 };
-
-//IF VALIDATED CREATE AN OBJECT AND ADD TO ARRAY
 
 //FORM VALUE SELECTOR
 const title = document.querySelector('#book_title');
@@ -188,10 +193,20 @@ const getFormValues = function () {
     const inputGenre = genre.value;
 
     const inputRead = statusRadio();
-    console.log(inputRead);
 
-    // constructor(id, title, firstName, lastName, pages, genre, date, status, comments)
     return [inputTitle, inputFirstName, inputLastName, inputPages, inputGenre, inputDate, inputRead, inputComments];
+};
+
+const setFormValues = function (book) {
+    title.value = book.title;
+    firstName.value = book.firstName;
+    lastName.value = book.lastName;
+    date.value = book.date;
+    pages.value = book.pages;
+    comments.value = book.comments;
+    genre.value = book.genre;
+
+    // how to set radio button checked from javascript
 };
 
 // FUNCTION: HELPER => GET READ STATUS VALUE
@@ -261,7 +276,6 @@ const submitForm2 = function (event) {
 
 // BOOK-READ-STATUS-BTN
 const toggleStatus = function (event) {
-    // need to access its parent and retrieve data-id and change data on the actual object
     if (event.target.classList.contains('slider')) {
         const id = Number.parseFloat(event.target.closest('.book').dataset.id);
         const changeBookRead = library.findIndex((book) => +book.id === id);
@@ -270,7 +284,7 @@ const toggleStatus = function (event) {
         console.log(library[changeBookRead].status);
     }
 };
-// DELETE-BTN TO DELETE THE BOOK FROM THE ARRAY
+
 // ASK THE USER ONE MORE TIME TO MAKE SURE THAT THEY WANT TO DELETE THIS
 const deleteBook = function (event) {
     if (event.target.classList.contains('fa-times-circle')) {
@@ -284,8 +298,37 @@ const deleteBook = function (event) {
     }
 };
 
-// BOOK-TITLE-BTN TO OPEN UP THE FORM AGAIN TO BE ABLE TO EDIT THE CARD
+//------------------------------ADD EVENT LISTENER  -------------------------------//
+//FUNCTION: OPEN THE FORM
+btnOpenForm.addEventListener('click', openForm);
+
+document.addEventListener('keydown', closeForm2);
+
+//FUNCTION: CLOSE THE FORM
+btnsCloseForm.forEach((btnClose) => {
+    btnClose.addEventListener('click', closeForm);
+});
+
+// FUNCTION: ADD-BOOK-BTN: OPEN UP THE FORM TO ADD TO THE LIBRARY
+btnSubmitForm.addEventListener('click', submitForm);
+document.addEventListener('keydown', submitForm2);
+
+// FUNCTION: BOOK-READ-STATUS-BTN
+booksCtn.addEventListener('click', toggleStatus);
+
+// btnDelete.addEventListener('click', deleteBook);
+booksCtn.addEventListener('click', deleteBook);
+
+// FUNCTION: BOOK-TITLE-BTN TO OPEN UP THE FORM AGAIN TO BE ABLE TO EDIT THE CARD
 //FORM SHOULD ALREADY BE FILLED OUT WITH SAVED DATA
+//USE THE SAME FOR AS ADD-BOOK BUTTON
+
+// console.log(doYouPray);
+// library.push(doYouPray);
+// library.push(doYouPray1);
+// library.push(doYouPray2);
+// library.push(doYouPray3);
+displayBook(library);
 
 //====EXTRA====//
 // ADD FILTERING FUNCTIONALITY THAT DISPLAYS BOOK BASED ON READ AND UNREAD STATUS
@@ -295,42 +338,6 @@ const deleteBook = function (event) {
 //UN-READ
 // ADD SORTING FUNCTIONALITY THAT DISPLAYS BOOK CARDS IN THE ALPHABETIZED ORDER (BY TITLE OR AUTHOR)
 
-//------------------------------ADD EVENT LISTENER  -------------------------------//
-//FUNCTION: OPEN THE FORM
-btnsOpenForm.forEach((btnOpen) => {
-    btnOpen.addEventListener('click', openForm);
-});
-document.addEventListener('keydown', closeForm2);
-
-//FUNCTION: CLOSE THE FORM
-btnsCloseForm.forEach((btnClose) => {
-    // console.log(btnClose);
-    btnClose.addEventListener('click', closeForm);
-});
-
-// FUNCTION: ADD-BOOK-BTN: OPEN UP THE FORM TO ADD TO THE LIBRARY
-btnSubmitForm.addEventListener('click', submitForm);
-document.addEventListener('keydown', submitForm2);
-
-// FUNCTION: BOOK-READ-STATUS-BTN
-//TOGGLES THE BOOK OBJECTS STATUS DATA
-booksCtn.addEventListener('click', toggleStatus);
-
-// FUNCTION: DELETE-BTN TO DELETE THE BOOK FROM THE ARRAY
-// ASK THE USER ONE MORE TIME TO MAKE SURE THAT THEY WANT TO DELETE THIS
-// IF TRUE, REMOVE THE BOOK FROM THE ARRAY
-// DISPLAY UPDATED ARRAY TO THE SCREEN
-
-// btnDelete.addEventListener('click', deleteBook);
-booksCtn.addEventListener('click', deleteBook);
-
-// FUNCTION: BOOK-TITLE-BTN TO OPEN UP THE FORM AGAIN TO BE ABLE TO EDIT THE CARD
-//FORM SHOULD ALREADY BE FILLED OUT WITH SAVED DATA
-//USE THE SAME FOR AS ADD-BOOK BUTTON
-
-console.log(doYouPray);
-library.push(doYouPray);
-library.push(doYouPray1);
-library.push(doYouPray2);
-library.push(doYouPray3);
-displayBook(library);
+// NEED TO RESET INPUT RADIO BUTTON TO NO
+//NEED TO RESET INPUT RADIO BUTTON TO NO
+// FUNCTION: REOPEN FORM
